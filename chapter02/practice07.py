09336ffe77016c812bf72f5d3f0480b920b19f2f
bea816 <kimnadon@naver.com>
2024-04-18 00:24:22 +0900

    챕터2

---
+++ chapter02/practice07.py
@@ -0,0 +1,7 @@
+a = int(input("숫자1 입력: "))
+b = int(input("숫자2 입력: "))
+c = int(input("숫자3 입력: "))
+result_add = a + b + c
+result_mul = a * b * c
+print(a, " + ", b, " + ", c, " = ", result_add)
+print(a, " * ", b, " * ", c, " = ", result_mul)
